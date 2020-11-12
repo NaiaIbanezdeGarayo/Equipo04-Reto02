876b2fe2aa06706a2e8d4c8b5bc9334b52755435
Naia <naia.ibanezdegarayo@ikasle.egibide.org>
2020-11-12 10:27:59 +0100

    inicializacionProyecto

---
+++ php/footer.view.php
@@ -0,0 +1,23 @@
+<footer>
+    <ul id="listageneral">
+        <ul id="contacto">
+            <li class="titulo">Contactanos</li>
+            <ul id="listacontacto">
+                <li><i class="fas fa-phone-alt"></i> 945 010 110</li>
+                <li><i class="fas fa-map-marker-alt"></i> c/Pozoa s/n (01013)</li>
+                <li><i class="fas fa-envelope"></i> arriaga@egibide.org</li>
+            </ul>
+        </ul>
+        <ul id="rs">
+            <li class="titulo">Siguenos</li>
+            <ul id="listars">
+                <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+                <li><a href="#"><i class="fab fa-facebook-square"></i></a></li>
+            </ul>
+        </ul>
+
+    </ul>
+</footer>
+<?php
+
---
+++ php/header.view.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en" xmlns="http://www.w3.org/1999/html">
+<head>
+    <title>AERGIBIDE SL</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" integrity="sha512-bLT0Qm9VnAYZDflyKcBaQ2gg0hSYNQrJ8RilYldYQ1FxQYoCLtUjuuRuZo+fjqhx/qtq/1itJ0C2ejDxltZVFg==" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+    <script src="../js/header.js"></script>
+    <link rel="stylesheet" href="../html/css/header.css">
+    <link rel="stylesheet" href="../html/css/footer.css">
+</head>
+<body>
+<header>
+    <nav id="navigation">
+        <input type="checkbox" id="check">
+        <label for="check" class="checkbtn open">
+            <i class="fas fa-ellipsis-v"></i>
+        </label>
+        <label for="check" class="checkbtn close">
+            <i class="fas fa-ellipsis-h"></i>
+        </label>
+        <ul class="menu">
+            <li><a href="header.view.php">Inicio</a></li>
+            <li><a href="#">Sobre nosotros</a></li>
+        </ul>
+        <p>AERGIBIDE SL</p>
+        <div class="dropdown">
+            <button onclick="myFunction()" class="dropbtn">
+                <i class="fas fa-user"></i>
+            </button>
+            <div class="optperfil-content" id="myDropdown">
+                    <a href="#">Perfil</a>
+                    <a href="#">Notificaciones</a>
+                    <a href="#">Configuración</a>
+                    <a href="#"><i class="fas fa-sign-out-alt"></i></a>
+            </div>
+        </div>
+    </nav>
+
+</header>
+
+<?php
