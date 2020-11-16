a9ab55f7afee9db97e9be2a7bbf7ea6667dc089d
Naia <naia.ibanezdegarayo@ikasle.egibide.org>
2020-11-16 09:04:06 +0100

    fn-cambios-9:04

--- html/header.view.php
+++ html/header.view.php
@@ -10,6 +10,7 @@
     <script src="../js/header.js"></script>
     <link rel="stylesheet" href="css/header.css">
     <link rel="stylesheet" href="css/footer.css">
+    <link rel="stylesheet" href="css/preguntas.css">
 </head>
 <body>
 <header>
