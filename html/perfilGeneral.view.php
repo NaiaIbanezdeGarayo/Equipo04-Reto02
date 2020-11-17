782b23e0e6488be540954bfa4508721376b67693
Naia <naia.ibanezdegarayo@ikasle.egibide.org>
2020-11-17 09:30:08 +0100

    fn-cambios-9:30

--- html/perfilGeneral.view.php
+++ html/perfilGeneral.view.php
@@ -12,23 +12,23 @@
     <section class="sectionPreg">
         <article id="articleMenu">
             <div class="items">
-                <button class="item item1"><a href="">GENERAL</a></button>
-                <button class="item item2"><a href=""><i class="fas fa-star"></i></a></button>
+                <button class="item item1" onclick="loadDoc()"><a href="">GENERAL</a></button>
+                <button class="item item2" onclick="loadDoc()"><a href=""><i class="fas fa-star"></i></a></button>
             </div>
         </article>
-        <article>
+        <article class="artPreg">
             <a href="http://www.google.es" class="tituloPreg">¿Título de una pregunta?</a>
             <div class="divDescripcion">
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
             </div>
         </article>
-        <article>
+        <article class="artPreg">
             <a href="http://www.google.es" class="tituloPreg">¿Título de una pregunta?</a>
             <div class="divDescripcion">
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
             </div>
         </article>
-        <article>
+        <article class="artPreg">
             <a href="http://www.google.es" class="tituloPreg">¿Título de una pregunta?</a>
             <div class="divDescripcion">
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
