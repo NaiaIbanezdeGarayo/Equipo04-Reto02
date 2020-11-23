4e1cf490f1591bd7837bfe5da5f3f1b876a29032
EricMartinezEgibide <eric.martinez@ikasle.egibide.org>
2020-11-23 12:20:40 +0100

    fe Adecentando

--- html/perfil.view.php
+++ html/perfil.view.php
@@ -11,6 +11,7 @@
     <div id="divGeneral">
         <section id="sectionGeneral">
             <article id="articleDatosUsuario">
+                <label id="lbNickname">Nickname</label>
                 <img src="../img/default-user-image.png">
                 <a href="../php/editarPerfil.php">Editar perfil <i class="fas fa-pencil-alt"></i></a>
                 <p>Nombre Apellido</p>
@@ -18,31 +19,50 @@
                 <p>Descripción</p>
             </article>
         </section>
-        <section class="sectionPreg">
+        <section class="sectionIzq">
+
             <article id="articleMenu">
                 <div class="items">
                     <button class="item item1" onclick="loadDoc()"><a href="">GENERAL</a></button>
                     <button class="item item2" onclick="loadDoc()"><a href=""><i class="fas fa-star"></i></a></button>
                 </div>
             </article>
-            <article class="artPreg">
-                <a href="http://www.google.es" class="tituloPreg">¿Título de una pregunta?</a>
-                <div class="divDescripcion">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                </div>
-            </article>
-            <article class="artPreg">
-                <a href="http://www.google.es" class="tituloPreg">¿Título de una pregunta?</a>
-                <div class="divDescripcion">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                </div>
-            </article>
-            <article class="artPreg">
-                <a href="http://www.google.es" class="tituloPreg">¿Título de una pregunta?</a>
-                <div class="divDescripcion">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                </div>
-            </article>
+
+            <div id="divPreguntas">
+                <article class="artPreg">
+                    <a href="http://www.google.es" class="tituloPreg">¿Título de una pregunta?</a>
+                    <div class="divDescripcion">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    </div>
+                </article>
+
+                <article class="artPreg">
+                    <a href="http://www.google.es" class="tituloPreg">¿Título de una pregunta?</a>
+                    <div class="divDescripcion">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    </div>
+                </article>
+
+                <article class="artPreg">
+                    <a href="http://www.google.es" class="tituloPreg">¿Título de una pregunta?</a>
+                    <div class="divDescripcion">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    </div>
+                </article>
+
+                <article class="artPreg">
+                    <a href="http://www.google.es" class="tituloPreg">¿Título de una pregunta?</a>
+                    <div class="divDescripcion">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    </div>
+                </article>
+
+
+            </div>
+
+
+
+
         </section>
     </div>
 
