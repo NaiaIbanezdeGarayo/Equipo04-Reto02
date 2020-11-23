b6a726c74661098cc706e8c8f720817087706f8b
EricMartinezEgibide <eric.martinez@ikasle.egibide.org>
2020-11-23 12:48:49 +0100

    fe-12:48 Perfil reestructurado para la implementación de datos automáticos.

--- html/perfil.view.php
+++ html/perfil.view.php
@@ -8,62 +8,47 @@
 </head>
 
 <body>
-    <div id="divGeneral">
-        <section id="sectionGeneral">
-            <article id="articleDatosUsuario">
-                <label id="lbNickname">Nickname</label>
-                <img src="../img/default-user-image.png">
-                <a href="../php/editarPerfil.php">Editar perfil <i class="fas fa-pencil-alt"></i></a>
-                <p>Nombre Apellido</p>
-                <p>Usuario</p>
-                <p>Descripción</p>
-            </article>
-        </section>
-        <section class="sectionIzq">
 
-            <article id="articleMenu">
-                <div class="items">
-                    <button class="item item1" onclick="loadDoc()"><a href="">GENERAL</a></button>
-                    <button class="item item2" onclick="loadDoc()"><a href=""><i class="fas fa-star"></i></a></button>
-                </div>
-            </article>
+    <div id="divPrincipal">
 
-            <div id="divPreguntas">
-                <article class="artPreg">
-                    <a href="http://www.google.es" class="tituloPreg">¿Título de una pregunta?</a>
-                    <div class="divDescripcion">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                    </div>
-                </article>
-
-                <article class="artPreg">
-                    <a href="http://www.google.es" class="tituloPreg">¿Título de una pregunta?</a>
-                    <div class="divDescripcion">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                    </div>
-                </article>
-
-                <article class="artPreg">
-                    <a href="http://www.google.es" class="tituloPreg">¿Título de una pregunta?</a>
-                    <div class="divDescripcion">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                    </div>
-                </article>
-
-                <article class="artPreg">
-                    <a href="http://www.google.es" class="tituloPreg">¿Título de una pregunta?</a>
-                    <div class="divDescripcion">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                    </div>
-                </article>
-
-
+        <div id="divIzq">
+            <div id="divBotones">
+                <button>General</button>
+                <button>Favoritos</button>
             </div>
 
+            <div id="divPreguntas">
 
+                <div class="divPregunta">
+                    <a href="http://www.google.es" class="tituloPreg">¿Título de una pregunta?</a>
+                    <div class="divDescripcion">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    </div>
+                </div>
 
+                <div class="divPregunta">
+                    <a href="http://www.google.es" class="tituloPreg">¿Título de una pregunta?</a>
+                    <div class="divDescripcion">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    </div>
+                </div>
 
-        </section>
+                <div class="divPregunta">
+                    <a href="http://www.google.es" class="tituloPreg">¿Título de una pregunta?</a>
+                    <div class="divDescripcion">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+        <div id="divDer">
+            <label id="lbNickname">Jaimito78</label>
+            <img id="imgPerfil" src="../img/default-user-image.png">
+            <label id="lbNombre">Jaimito Pérez</label>
+            <label id="lbDescripcion">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</label>
+        </div>
     </div>
 
 </body>
