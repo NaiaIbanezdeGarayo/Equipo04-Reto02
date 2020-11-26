f6826ac79255ee6f144b807c7f8a349f6ccea8b5
EricMartinezEgibide <eric.martinez@ikasle.egibide.org>
2020-11-26 08:29:17 +0100

    fe-8:28 Filtro de preguntas (En progreso)

---
+++ js/preguntas.js
@@ -0,0 +1,17 @@
+$('#slOrden').on('change', function (e) {
+    var optionSelected = $("option:selected", this);
+    var valueSelected = this.value;
+
+    switch (valueSelected){
+
+        case "fechaDesc":
+
+            break;
+
+        case "fechaAsc":
+
+            break;
+
+    }
+
+});
